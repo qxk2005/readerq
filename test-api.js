09c7538f2eda3b689a87c751892354ed734983db
qiuyang <qiuyang@bingosoft.net>
2026-06-25 01:32:53 +0800

    取消不必要的 test 文件

--- test-api.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-fetch('https://readwise.io/api/v3/list/?limit=1', {
-  headers: { 'Authorization': `Token ${process.env.READWISE_TOKEN}` }
-})
-.then(res => res.json())
-.then(data => console.log(JSON.stringify(Object.keys(data))))
-.catch(console.error);
