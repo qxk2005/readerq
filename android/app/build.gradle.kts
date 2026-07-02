ef91daee4a4bc2923ef9d714cbe280ada2d10e42
qiuyang <qiuyang@bingosoft.net>
2026-07-02 16:58:31 +0800

    chore: bump version to 1.0.25

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.readerq.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 25
+        versionName = "1.0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
