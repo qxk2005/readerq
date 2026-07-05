40ecb7de580d1d74b6bc21aa480e91f7f0a99735
qiuyang <qiuyang@bingosoft.net>
2026-07-05 20:08:59 +0800

    chore: 版本号升级到 v1.0.38

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "readerq.qiuyang.ai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "1.0.37"
+        versionCode = 38
+        versionName = "1.0.38"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
