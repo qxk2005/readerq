fe34b720035c577d0fe07984a57b6c52108dc262
qiuyang <qiuyang@bingosoft.net>
2026-07-07 18:58:19 +0800

    chore: 升级版本至 v1.0.41

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "readerq.qiuyang.ai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "1.0.40"
+        versionCode = 41
+        versionName = "1.0.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
